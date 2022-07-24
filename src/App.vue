4107c34ed95abd8041103c3d59bc1853f1fbdf08
wardlin <wardlin@tencent.com>
2022-07-24 09:18:15 +0800

    去掉菜单栏+增加备案号显示

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
     <router-view />
+    <div
+      style="position:fixed;text-align:center;bottom:0;margin:0 auto;width:100%;color: #5c6b77"
+    >
+      <a
+        target="_blank"
+        style="color: #5c6b77"
+        href="https://beian.miit.gov.cn/"
+      >粤ICP备2022087079号</a
+      >
+      &nbsp; @2022 提醒助手
+    </div>
   </div>
 </template>
 
